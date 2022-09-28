fbd7e0695001e2006dfe09f1f1f3f5690499b6c8
Theorafael <Theorafaeltitahena@gmail.com>
2022-09-28 15:33:30 +0700

    280922_basic PHP

---
+++ comment_1.php
@@ -0,0 +1,4 @@
+<?php
+echo "<p>Halo Dunia!</p>";
+echo "<p>Ini program pertama saya di PHP</p>";
+?>
---
+++ comment_2.php
@@ -0,0 +1,5 @@
+<?php
+echo "<p>Saya belajar PHP</p> \n";
+echo "<p>Ini baris ke dua</p> \n";
+echo "<p>Ini baris ke tiga</p>";
+?>
